911d0484399bee0de401c586b0dca2957970aba4
dmitriy <dirsherman8@gmail.com>
2022-02-17 16:59:48 +0200

    created models Product and Comment

--- shop/models.py
+++ shop/models.py
@@ -2,12 +2,29 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     count_products = models.IntegerField()
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=30, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     count_products = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    category = models.ManyToManyField(Category, related_name='category_product')
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='brand_product')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+
+
+
+
+
