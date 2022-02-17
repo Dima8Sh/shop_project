ef64e944667ee256bc7ba476431a50bbe00412d6
dmitriy <dirsherman8@gmail.com>
2022-02-17 13:02:20 +0200

    created models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    count_products = models.IntegerField()
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=30, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    count_products = models.IntegerField()
