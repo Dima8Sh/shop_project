7afec3a16ae4dbf383a6fa09b7b10883ba55b411
dmitriy <dirsherman8@gmail.com>
2022-02-21 17:25:38 +0200

    created factories

---
+++ shop/tests/factories.py
@@ -0,0 +1,36 @@
+import factory
+
+from shop.models import Category, Product, Brand
+
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    title = factory.faker.Faker('name')
+
+
+class BrandFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Brand
+
+    title = factory.faker.Faker('name')
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+
+    name = factory.faker.Faker('name')
+    brand = factory.SubFactory(BrandFactory)
+
+    @factory.post_generation
+    def categories(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for category in extracted:
+                self.categories.append(category)
+
